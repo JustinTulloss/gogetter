c29e6875d290fd5d91307a2fddacf5051db8655d
Justin Tulloss <justin.tulloss@gmail.com>
2014-07-14 12:41:54 -0700

    Preliminary tests for og tag parsing

---
+++ gogetter_test.go
@@ -0,0 +1,59 @@
+package main
+
+import "reflect"
+import "strings"
+import "testing"
+
+type doctest struct {
+	doc string
+	tags map[string] string
+}
+
+var testdocs = []doctest{
+	{
+		`<html>
+			<head>
+				<title>Not interesting</title>
+				<meta property="og:title" content="More interesting" />
+			</head>
+		</html>`,
+		map[string]string{
+			"og:title": "More interesting",
+		},
+	},
+	{
+		`random gobbleygook
+		<title>not interesting</title>
+		<meta property="og:title" content="relevant" />`,
+		map[string]string{
+			"og:title": "relevant",
+		},
+	},
+	{
+		`<meta property="twitter:hello" content="twitter works" />`,
+		map[string]string{
+			"twitter:hello": "twitter works",
+		},
+	},
+	{
+		`<meta property="airbedandbreakfast:test" content="airbnb works" />`,
+		map[string]string{
+			"airbedandbreakfast:test": "airbnb works",
+		},
+	},
+}
+
+
+func TestParseTags(t *testing.T) {
+	t.Parallel()
+	for _, doctest := range testdocs {
+		testresult, err:= parseTags(strings.NewReader(doctest.doc))
+		if err != nil {
+			t.Errorf(err.Error())
+		}
+		eq := reflect.DeepEqual(testresult, doctest.tags)
+		if !eq {
+			t.Errorf("%v != %v", testresult, doctest.tags)
+		}
+	}
+}
