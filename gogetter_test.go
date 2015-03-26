42af1ad5d085dceadcf6ae45bbb25bac6d8119c4
Justin Tulloss <justin.tulloss@gmail.com>
2015-03-26 15:38:20 -0700

    Fix tests for new formats

--- gogetter_test.go
+++ gogetter_test.go
@@ -1,12 +1,20 @@
 package gogetter
 
-import "reflect"
+import (
+	"reflect"
+
+	"github.com/JustinTulloss/gogetter/applink"
+	"github.com/JustinTulloss/gogetter/wildcard"
+)
 import "strings"
-import "testing"
+import (
+	"testing"
+	"time"
+)
 
 type doctest struct {
 	doc  string
-	tags map[string]string
+	tags interface{}
 }
 
 var testdocs = []doctest{
@@ -17,36 +25,80 @@ var testdocs = []doctest{
 				<meta property="og:title" content="More interesting" />
 			</head>
 		</html>`,
-		map[string]string{
-			"og:title": "More interesting",
-			"title":    "Not interesting",
+		&wildcard.LinkCard{
+			Card: wildcard.Card{
+				CardType: wildcard.LinkType,
+			},
+			Target: &wildcard.LinkTarget{
+				GenericMetadata: wildcard.GenericMetadata{
+					Title: "More interesting",
+					AppLink: &applink.AppLink{
+						Ios:     &applink.Ios{},
+						Iphone:  &applink.Iphone{},
+						Ipad:    &applink.Ipad{},
+						Android: &applink.Android{},
+					},
+					Image:           &wildcard.ImageDetails{},
+					PublicationDate: &time.Time{},
+				},
+			},
 		},
 	},
 	{
 		`random gobbleygook
 		<title>not interesting</title>
 		<meta property="og:title" content="relevant" />`,
-		map[string]string{
-			"og:title": "relevant",
-			"title":    "not interesting",
+		&wildcard.LinkCard{
+			Card: wildcard.Card{
+				CardType: wildcard.LinkType,
+			},
+			Target: &wildcard.LinkTarget{
+				GenericMetadata: wildcard.GenericMetadata{
+					Title: "relevant",
+					AppLink: &applink.AppLink{
+						Ios:     &applink.Ios{},
+						Iphone:  &applink.Iphone{},
+						Ipad:    &applink.Ipad{},
+						Android: &applink.Android{},
+					},
+					Image:           &wildcard.ImageDetails{},
+					PublicationDate: &time.Time{},
+				},
+			},
 		},
 	},
-	{
-		`<meta property="twitter:hello" content="twitter works" />`,
-		map[string]string{
-			"twitter:hello": "twitter works",
-		},
-	},
-	{
-		`<meta property="airbedandbreakfast:test" content="airbnb works" />`,
-		map[string]string{
-			"airbedandbreakfast:test": "airbnb works",
-		},
-	},
+	/*
+			{
+				`<meta property="twitter:hello" content="twitter works" />`,
+				map[string]string{
+					"twitter:hello": "twitter works",
+				},
+			},
+		{
+			`<meta property="airbedandbreakfast:test" content="airbnb works" />`,
+			map[string]string{
+				"airbedandbreakfast:test": "airbnb works",
+			},
+		},*/
 	{
 		`<meta name="description" content="pod (plain old descriptions) work" />`,
-		map[string]string{
-			"description": "pod (plain old descriptions) work",
+		&wildcard.LinkCard{
+			Card: wildcard.Card{
+				CardType: wildcard.LinkType,
+			},
+			Target: &wildcard.LinkTarget{
+				Description: "pod (plain old descriptions) work",
+				GenericMetadata: wildcard.GenericMetadata{
+					AppLink: &applink.AppLink{
+						Ios:     &applink.Ios{},
+						Iphone:  &applink.Iphone{},
+						Ipad:    &applink.Ipad{},
+						Android: &applink.Android{},
+					},
+					Image:           &wildcard.ImageDetails{},
+					PublicationDate: &time.Time{},
+				},
+			},
 		},
 	},
 }
@@ -59,13 +111,13 @@ func TestParseTags(t *testing.T) {
 		t.Fail()
 	}
 	for _, doctest := range testdocs {
-		testresult, err := scraper.ParseTags(strings.NewReader(doctest.doc))
+		testresult, err := scraper.ParseTags(strings.NewReader(doctest.doc), "")
 		if err != nil {
 			t.Errorf(err.Error())
 		}
 		eq := reflect.DeepEqual(testresult, doctest.tags)
 		if !eq {
-			t.Errorf("%v != %v", testresult, doctest.tags)
+			t.Errorf("%#v != %#v", testresult, doctest.tags)
 		}
 	}
 }
